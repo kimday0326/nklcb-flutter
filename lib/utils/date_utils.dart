3eee37387f68493403711905d0c4fa52e4de07fc
kimday0326 <ktnqkr01@gmail.com>
2024-07-09 17:00:05 +0900

    feat: DateUtils 추가

---
+++ lib/utils/date_utils.dart
@@ -0,0 +1,11 @@
+import 'package:intl/intl.dart';
+
+class DateUtils {
+  static String formatDate(DateTime date, {bool includeTime = false}) {
+    if (includeTime) {
+      return DateFormat('yyyy-MM-dd HH:mm').format(date); // 년월일 시분 형식
+    } else {
+      return DateFormat('yyyy-MM-dd').format(date); // 년월일 형식
+    }
+  }
+}
